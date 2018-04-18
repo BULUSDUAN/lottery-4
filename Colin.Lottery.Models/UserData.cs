1ef861af0f3b87b6e2b6d3117de82b4b94a38c93
Colin <zhangcheng5468@gmail.com>
2018-04-18 20:39:09 +0800

    尝试搭建SignalR环境

---
+++ Colin.Lottery.Models/UserData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Colin.Lottery.Models
+{
+    public class UserData
+    {
+        public UserData(string connectionId, string userName) : this(connectionId, userName, true, DateTime.Now)
+        {
+        }
+
+        public UserData(string connectionId, string userName, bool isActive, DateTime connecteAt)
+        {
+            ConnectionId = connectionId;
+            UserName = userName;
+            IsActive = isActive;
+            ConnecteAt = connecteAt;
+        }
+
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ConnecteAt { get; set; }
+
+        public void GetOffLine(string connectionId)
+        {
+            this.IsActive = false;
+        }
+    }
+}
