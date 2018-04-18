9cc3cdcc26b113ab1804f1f9417f830a2bf4314e
Colin <zhangcheng5468@gmail.com>
2018-04-18 14:57:06 +0800

    整理代码

--- Colin.Lottery.Analyzers.Test/JinMaAnayyzerTest.cs
+++ Colin.Lottery.Analyzers.Test/JinMaAnayyzerTest.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Colin.Lottery.Models;
+using System;
+using Xunit;
+
 namespace Colin.Lottery.Analyzers.Test
 {
-    public class JinMaAnayyzerTest
+    public class JinMaAnalyzerTest
     {
-        public JinMaAnayyzerTest()
+        [Fact]
+        public async void GetForcastDataTest()
         {
+            var plans = await JinMaAnalyzer.Instance.GetForcastData();
+            ShowPlan(plans.Plan1);
+            ShowPlan(plans.Plan2);
+        }
+
+        void ShowPlan(IForcastPlanModel plan)
+        {
+            plan.ForcastData.ForEach(i =>
+            {
+                Console.WriteLine($"{i.PeriodRange} [{i.ForcastNo}] {i.LastPeriod} {i.DrawNo} {i.ChaseTimes} [{i.IsWin}]");
+            });
+        }
+
+        [Fact]
+        public async void CalcuteScoreTest()
+        {
+            var plans = await JinMaAnalyzer.Instance.GetForcastData();
+            JinMaAnalyzer.Instance.CalcuteScore(ref plans, true);
         }
     }
 }
