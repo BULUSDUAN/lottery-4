8b4d646f97fa1e1d154f6d17160ef7789efd3efc
Colin <zhangcheng5468@gmail.com>
2018-04-18 21:29:11 +0800

    更新Nuget版本并整理代码

---
+++ Colin.Lottery.StrategyService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Colin.Lottery.StrategyService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 or http://+:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            using (WebApp.Start<Startup>("http://localhost:8080/"))
+            {
+                Console.WriteLine("SignalR Server running at http://localhost:8080/");
+            }
+
+            Start();
+            Console.ReadKey();
+        }
+
+        async static void Start()
+        {
+            await JinMaStrategyService.Instance.Start();
+        }
+    }
+}
