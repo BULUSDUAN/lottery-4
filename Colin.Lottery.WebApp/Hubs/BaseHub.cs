de8878bb19ae87f1b1a3b058210bb562abd6f28d
Colin <zhangcheng5468@gmail.com>
2018-04-20 09:27:43 +0800

    升级项目架构到.net core 2.1

---
+++ Colin.Lottery.WebApp/Hubs/BaseHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;
+
+using Colin.Lottery.Models;
+
+
+namespace Colin.Lottery.WebApp.Hubs
+{
+    public abstract class BaseHub : Hub
+    {
+        /// <summary>
+        /// 用户统计
+        /// </summary>
+        protected static int _usersCount = 0;
+
+        /// <summary>
+        /// 在线用户
+        /// </summary>
+        protected static ConcurrentDictionary<string, object> _users = new ConcurrentDictionary<string, object>();
+
+        public async override Task OnConnectedAsync()
+        {
+            Interlocked.Increment(ref _usersCount);
+            _users[Context.ConnectionId] = new UserData(Context.ConnectionId, $"user{_usersCount}");
+
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            Interlocked.Decrement(ref _usersCount);
+            _users.TryRemove(Context.ConnectionId, out object user);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- Colin.Lottery.WebApp/Hubs/ChatHub.cs
+++ Colin.Lottery.WebApp/Hubs/ChatHub.cs
@@ -5,18 +5,8 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Colin.Lottery.WebApp.Hubs
 {
-    public class ChatHub : Hub
+    public class ChatHub : BaseHub
     {
-        public override async Task OnConnectedAsync()
-        {
-            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "joined");
-        }
-
-        public override async Task OnDisconnectedAsync(Exception ex)
-        {
-            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "left");
-        }
-
         public async Task Send(string message)
         {
             await Clients.All.SendAsync("SendMessage", Context.User.Identity.Name, message);
