8259fbc89fe1c15b3149ddaace925c35e2fe077c
Colin <zhangcheng5468@gmail.com>
2018-05-10 19:05:14 +0800

    1.添加logo 2.修改通知消息显示方式

--- Colin.Lottery.WebApp/wwwroot/js/site.js
+++ Colin.Lottery.WebApp/wwwroot/js/site.js
@@ -8,17 +8,19 @@
         });
     })();
 
-    /*
     //消息通知
     (function () {
         //创建连接
         let hub = '/hubs/notify';
-        let connection = new signalR.HubConnection(hub);
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(hub)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
 
         //启动连接并初始化数据
         connection.start().then(
             function () {
-                connection.invoke('GetNotifications', 80, true);
+                connection.invoke('GetNotifications', 100, true);
             },
             function () {
                 console.error("服务器(" + hub + ")连接失败");
@@ -33,15 +35,43 @@
                 return;
 
             for (let i = msgs.length - 1; i >= 0; i--) {
-                $.notify(msgs[i], "success");
+                notify('高分提醒', msgs[i]);
             }
         });
     })();
-    */
 })();
 
 //获取URL中最后一个参数
 function getLastParameter() {
     return location.pathname.split('/').pop();
+}
+
+function notify(title, content) {
+    if (!title || !content) {
+        console.error('消息通知参数错误');
+        return;
+    }
+
+    let options = {body: content, icon: '/images/logo.png'};
+
+    // 先检查浏览器是否支持
+    if (!("Notification" in window))
+        alert("抱歉，您的浏览器不支持通知提醒");
+
+    // 检查用户是否同意接受通知
+    else if (Notification.permission === "granted")
+        var notification = new Notification(title, options);
+
+    // 否则我们需要向用户获取权限
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            // 如果用户同意，就可以向他们发送通知
+            if (permission === "granted") {
+                var notification = new Notification(title, options);
+            }
+        });
+    }
+    // 最后，如果执行到这里，说明用户已经拒绝对相关通知进行授权
+    // 出于尊重，我们不应该再打扰他们了
 }
 
