f998e6c41da24b96627c807e65ca0b5ede861daf
Colin <zhangcheng5468@gmail.com>
2018-06-08 19:26:18 +0800

    测试邮件发送

--- Colin.Lottery.AutoBetService/Program.cs
+++ Colin.Lottery.AutoBetService/Program.cs
@@ -18,17 +18,21 @@ namespace Colin.Lottery.AutoBetService
         static void Main(string[] args)
         {
             Test();
-            
+
             Console.WriteLine("OK");
-            
+
             Console.ReadKey();
         }
 
         private static async void Test()
         {
-            var plans = await JinMaAnalyzer.Instance.GetForcastData(LotteryType.Pk10, 2);
-            JinMaAnalyzer.Instance.CalcuteScore(ref plans, true);
-            await MailNotify.NotifyAsync(new MailNotifyModel(LotteryType.Pk10, 2, Plan.PlanB,plans.Plan2, plans.Plan1.ForcastDrawNo));
+            //var plans = await JinMaAnalyzer.Instance.GetForcastData(LotteryType.Pk10, 2);
+            //JinMaAnalyzer.Instance.CalcuteScore(ref plans, true);
+            //await MailNotify.NotifyAsync(new MailNotifyModel(LotteryType.Pk10, 2, Plan.PlanB,plans.Plan2, plans.Plan1.ForcastDrawNo));
+            
+            var config = ConfigUtil.GetAppSettings<MailNotifyConfig>("MailNotify");
+            await MailUtil.MailAsync(config.From, config.To.Split(','), config.Subject, "测试邮件发送", config.ContentType,
+                config.SmtpHost, config.SmtpPort, config.UserName, config.Password);
         }
     }
 }
