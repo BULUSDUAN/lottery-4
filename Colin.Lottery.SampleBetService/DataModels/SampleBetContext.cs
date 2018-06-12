a6e5a1c4070904a5209ce3b3f892177717057505
Colin <zhangcheng5468@gmail.com>
2018-06-12 11:19:11 +0800

    提交遗漏文件

---
+++ Colin.Lottery.SampleBetService/DataModels/SampleBetContext.cs
@@ -0,0 +1,34 @@
+﻿using Colin.Lottery.Models;
+using Microsoft.EntityFrameworkCore;
+
+using Colin.Lottery.Models.BetService;
+using Colin.Lottery.Utils;
+using Microsoft.Extensions.Configuration;
+
+namespace Colin.Lottery.SampleBetService.DataModels
+{
+    public class SampleBetContext : DbContext
+    {
+        public DbSet<BetGuaRecord> BetGua { get; set; }
+        public DbSet<BetAll3Record> BetAll3 { get; set; }
+        public DbSet<BetAllRecord> BetAll { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConfigUtil.Configuration["ConnectionString"]);
+        }
+    }
+
+    public class BetGuaRecord : BetRecordModel
+    {
+        public BetGuaRecord(LotteryType lottery,int rule,Plan plan,long periodNo,string betNo,int chaseTimes,decimal betMoney,float odds,decimal balance):base(lottery,rule,plan,periodNo,betNo,chaseTimes,betMoney,odds,balance){}
+    }
+    public class BetAll3Record : BetRecordModel
+    {
+        public BetAll3Record(LotteryType lottery,int rule,Plan plan,long periodNo,string betNo,int chaseTimes,decimal betMoney,float odds,decimal balance):base(lottery,rule,plan,periodNo,betNo,chaseTimes,betMoney,odds,balance){}
+    }
+    public class BetAllRecord : BetRecordModel
+    {
+        public BetAllRecord(LotteryType lottery,int rule,Plan plan,long periodNo,string betNo,int chaseTimes,decimal betMoney,float odds,decimal balance):base(lottery,rule,plan,periodNo,betNo,chaseTimes,betMoney,odds,balance){}
+    }
+}
