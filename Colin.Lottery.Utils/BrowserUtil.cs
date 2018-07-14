a652b26c0bfe4272a86fad9e66bf423c25265af4
songzheng45 <songzheng45@126.com>
2018-07-14 14:54:39 +0800

    重构 BrowserUtil，添加 Quit() 方法

--- Colin.Lottery.Utils/BrowserUtil.cs
+++ Colin.Lottery.Utils/BrowserUtil.cs
@@ -2,11 +2,9 @@
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Threading;
-
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-
 using Colin.Lottery.Models;
 using Colin.Lottery.Models.BrowserModels;
 
@@ -14,19 +12,45 @@ namespace Colin.Lottery.Utils
 {
     public class BrowserUtil
     {
+        private ChromeDriver _chromeDriver;
+        private ChromeOptions _chromeOptions;
+
+        public BrowserUtil()
+            : this(null)
+        {
+        }
+
+        public BrowserUtil(Proxy proxy)
+        {
+            if (proxy != null)
+            {
+                ChromeOptions.Proxy = proxy;
+            }
+
+            _chromeDriver = new ChromeDriver(ChromeOptions);
+        }
+
+        private ChromeOptions ChromeOptions
+        {
+            get
+            {
+                if (_chromeOptions != null) return _chromeOptions;
+
+                _chromeOptions = new ChromeOptions();
+                _chromeOptions.AddArguments("headless", "disable-gpu", "disable-infobars", "--disable-extensions");
+                return _chromeOptions;
+            }
+        }
+
         /// <summary>
         /// 开始访问前
         /// </summary>
         public event EventHandler<ExploreStartingEventArgs> Starting;
+
         /// <summary>
         /// 访问结束后
         /// </summary>
         public event EventHandler<ExploreCompleteEventArgs> Completed;
-        /// <summary>
-        /// 代理
-        /// </summary>
-        /// <value>The proxy.</value>
-        public Proxy Proxy { get; set; }
 
         /// <summary>
         /// 使用Headless Chrome访问给定地址
@@ -44,44 +68,41 @@ namespace Colin.Lottery.Utils
                 var watch = new Stopwatch();
                 watch.Start();
 
-                var options = new ChromeOptions();
-                options.AddArguments("headless", "disable-gpu", "disable-infobars", "--disable-extensions");
-                if (Proxy != null)
-                    options.Proxy = Proxy;
-
                 Starting?.Invoke(this, new ExploreStartingEventArgs(url));
                 try
                 {
-                    using (var chrome = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, options))
-                    {
-                        //请求URL
-                        chrome.Navigate().GoToUrl(url);
+//                    using (var chrome = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory, options))
+//                    {
+                    //请求URL
+                    _chromeDriver.Navigate().GoToUrl(url);
 
-                        if (operation != null)
+                    //执行JavaScript
+                    if (script != null)
+                        _chromeDriver.ExecuteAsyncScript(script.Code, script.Args);
+
+                    //返回条件和超时时间
+                    if (operation?.Condition != null)
+                    {
+                        var driverWait =
+                            new WebDriverWait(_chromeDriver, TimeSpan.FromMilliseconds(operation.Timeout));
+                        driverWait.Until(driver =>
                         {
                             //执行预操作
-                            operation.Action?.Invoke(chrome);
+                            operation.Action?.Invoke(_chromeDriver);
 
-                            //返回条件和超时时间
-                            if (operation.Condition != null)
-                            {
-                                var driverWait = new WebDriverWait(chrome, TimeSpan.FromMilliseconds(operation.Timeout));
-                                driverWait.Until(operation.Condition);
-                            }
-                        }
-
-                        //执行JavaScript
-                        if (script != null)
-                            chrome.ExecuteAsyncScript(script.Code, script.Args);
-
-                        watch.Stop();
-
-                        var threadId = Thread.CurrentThread.ManagedThreadId;
-                        var elapsed = watch.ElapsedMilliseconds;
-                        var pageSource = chrome.PageSource;
-
-                        Completed?.Invoke(this, new ExploreCompleteEventArgs(url, chrome, threadId, elapsed, pageSource));
+                            return operation.Condition.Invoke(driver);
+                        });
                     }
+
+                    watch.Stop();
+
+                    var threadId = Thread.CurrentThread.ManagedThreadId;
+                    var elapsed = watch.ElapsedMilliseconds;
+                    var pageSource = _chromeDriver.PageSource;
+
+                    Completed?.Invoke(this,
+                        new ExploreCompleteEventArgs(url, _chromeDriver, threadId, elapsed, pageSource));
+//                    }
                 }
                 catch (Exception ex)
                 {
@@ -103,6 +124,11 @@ namespace Colin.Lottery.Utils
         public async Task Explore(string url)
         {
             await Explore(url, null, null);
+        }
+
+        public void Quit()
+        {
+            _chromeDriver.Quit();
         }
     }
 }
