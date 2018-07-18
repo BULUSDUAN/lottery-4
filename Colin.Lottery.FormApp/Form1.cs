fd22cd30777c848c54a6e0aa19b63cf9e4ef5351
Colin <zhangcheng5468@gmail.com>
2018-07-18 20:01:15 +0800

    整理代码

--- Colin.Lottery.FormApp/Form1.cs
+++ Colin.Lottery.FormApp/Form1.cs
@@ -1,51 +1,45 @@
-﻿using Colin.Lottery.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Colin.Lottery.Models; 
+using System; 
+using System.Collections.Generic; 
+using System.ComponentModel; 
+using System.Data; 
+using System.Diagnostics; 
+using System.Drawing; 
+using System.Linq; 
+using System.Text; 
+using System.Text.RegularExpressions; 
+using System.Threading; 
+using System.Threading.Tasks; 
+using System.Windows.Forms; 
 
-namespace Colin.Lottery.FormApp
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            CheckForIllegalCrossThreadCalls = false;
-            InitializeComponent();
-            browser.DocumentCompleted += Brower_DocumentCompleted;
+namespace Colin.Lottery.FormApp {
+    public partial class Form1:Form {
+        public Form1() {
+            CheckForIllegalCrossThreadCalls = false; 
+            InitializeComponent(); 
+            browser.DocumentCompleted + = Brower_DocumentCompleted; 
         }
 
-        private void Brower_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
+        private void Brower_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) {
             if (browser.ReadyState != WebBrowserReadyState.Complete)
-                return;
+                return; 
 
-            switch (browser.Url.LocalPath)
-            {
+            switch (browser.Url.LocalPath) {
                 case "/":
-                    ChooseRoutes();
-                    break;
+                    ChooseRoutes(); 
+                    break; 
                 case "/Home/TradeAgreements":
-                    AgreeAgreement();
-                    break;
+                    AgreeAgreement(); 
+                    break; 
                 default:
-                    break;
+                    break; 
             }
         }
 
         /// <summary>
         /// 选择线路
         /// </summary>
-        private void ChooseRoutes()
-        {
+        private void ChooseRoutes() {
             //C# 版本
             //browser.WaitUtil(
             //     wb => !string.IsNullOrWhiteSpace(wb.Invoke(new Func<string>(() => wb.Document.GetElementById("click_url_0").GetAttribute("onclick"))) as string),
@@ -60,33 +54,40 @@ let timer= setInterval(function(){
         $('#click_url_0').click();
         clearInterval(timer);
     }
-},1000);");
+},1000);"); 
         }
 
         /// <summary>
         /// 同意协议
         /// </summary>
-        private void AgreeAgreement()
-        {
+        private void AgreeAgreement() {
             //browser.Document.GetElementById("btn_OK").InvokeMember("click");
 
-            browser.ExecuteScript("$('#btn_OK').click()");
+            browser.ExecuteScript("$('#btn_OK').click()"); 
         }
 
 
 
         //彩种、玩法、期号、号码、下注金额
-        private void Bet(LotteryType type, int rule, long periodNo, string betNo, decimal betMoney)
-        {
-            var dictNames = new Dictionary<LotteryType, int>
-            {
-                [LotteryType.Pk10] = 19,
+        private void Bet(LotteryType type, int rule, long periodNo, string betNo, decimal betMoney) {
+            var dictNames = new Dictionary < LotteryType, int >  {
+                [LotteryType.Pk10] = 19, 
                 [LotteryType.Cqssc] = 1
-            };
+            }; 
 
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder(); 
             //选择彩种
-            var typeId = dictNames[type];
+            var typeId = dictNames[type]; 
+            sb.AppendLine(@$"var win=document.getElementById("mainFrame").contentWindow;
+
+//选择彩种
+var parts= win.location.pathname.split('/');
+var typeId= parts[parts.length-1];
+if(typeId!={typeId})
+{
+    parts[parts.length-1]={typeId};
+    win.location.pathname=parts.join('/');
+}")
         }
     }
 }
