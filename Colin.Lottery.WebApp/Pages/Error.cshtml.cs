fb82085294b92ac50a14a0424a48003168d29e01
ColinChang <zhangcheng5468@gmail.com>
2018-12-10 00:08:33 +0800

    升级.net core到2.2

--- Colin.Lottery.WebApp/Pages/Error.cshtml.cs
+++ Colin.Lottery.WebApp/Pages/Error.cshtml.cs
@@ -5,18 +5,16 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Colin.Lottery.WebApp.Pages
 {
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; private set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-
-            //Session
         }
     }
 }
